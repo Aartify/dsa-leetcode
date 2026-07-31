b47214ffba16eaf21603b7707df09412eb1b8ba9
Aarti <d.yadavaarti@gmail.com>
2026-07-31 08:29:20 +0530

    Time: 9 ms (94.22%), Space: 48 MB (73.7%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPushes(String word) {
+
+        int[] freq = new int[26];
+
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int ans = 0;
+        int index = 0;
+
+        // Traverse from largest frequency
+        for (int i = 25; i >= 0; i--) {
+
+            if (freq[i] == 0)
+                break;
+
+            int pushes = index / 8 + 1;
+            ans += freq[i] * pushes;
+            index++;
+        }
+
+        return ans;
+    }
+}
